672400a47b51977913c2d14c1e6413b3b47d4c10
CharlesShan-hub <charles.shht@gmail.com>
2021-04-18 20:59:46 +0800

    线性表练习题更新到第九题

--- 线性表/线性表-练习题.cpp
+++ 线性表/线性表-练习题.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define Maxsize 10
+#define Maxsize 20
 typedef struct{
 	int data[Maxsize];
 	int length;
@@ -175,22 +175,127 @@ bool DeleteInterval2(SqList &L,int s, int t){
 /* 从有序顺序表中删除所有与其值重复的元素, 
 	是比表中所有元素的值都不同*/
 
-bool DeleteDouble(SqList &L){
-	
+// 我这个程序跟答案相比, 兼容了表长为0的情况,
+// 而且是从下标为1开始向前对比的, 和答案不一样.
+bool DeleteSame1(SqList &L){
+	int count = 0;
+	for(int i;i<L.length;i++){
+		if(i == 0) continue;
+		if(L.data[i]==L.data[i-count-1])
+			count++;
+		else
+			L.data[i-count] = L.data[i];
+	}
+	L.length-=count;
+	return true;
 }
+
+// 答案方法: 使用了我第三题的i和j的方法
+// i在后边, j在前边
+bool DeleteSame2(SqList &L){
+	if(L.length == 0)
+		return false;
+	int i,j;
+	for(i=0,j=1; j<L.length; j++)
+		// 查找下一个与上一个值不同的元素
+		if(L.data[i]!=L.data[j])
+			// 找到后, 将元素前移
+			L.data[++i] = L.data[j];
+	L.length = i+1;
+	return true;
+}
+
+/* 将两个有序顺序表合并为一个新的有序顺序表,
+ 并由函数返回结果顺序表*/
+// 标准答案
+bool Merge(SqList A, SqList B, SqList &C){
+	// 将A与B merge入C
+	if(A.length + B.length > Maxsize){
+		printf("Not enough place!\n");
+		return false;
+	}
+	int i=0,j=0,k=0;
+	// 循环, 两两比较, 小者存入结果表
+	while(i<A.length && j<B.length)
+		C.data[k++] = A.data[i]<B.data[j]? A.data[i++]:B.data[j++];
+	// 还剩一个没有比较完的顺序表
+	while(i<A.length)
+		C.data[k++] = A.data[i++];
+	while(j<B.length)
+		C.data[k++] = B.data[j++];
+	C.length = A.length + B.length;
+	return true;
+}
+
+/* 已知在一维数组A[m+n]中依次存放两个线性表(a1,a2,a3 .. am)
+ 和(b1,b2,...bn).编写一个函数, 将数组中两个顺序表的位置互换.*/
+
+bool Exchange(SqList &L,int m,int n){
+	// abcde1234567
+	// 7654321edcba
+	// 1234567abcde
+	// First we inverse the all list
+	for(int i=0;i<L.length/2;i++){
+		// a  a+b b
+		// b  b   a
+		L.data[i] += L.data[L.length-i-1];
+		L.data[L.length-i-1] = L.data[i] - L.data[L.length-i-1];
+		L.data[i] -= L.data[L.length-i-1];
+	}
+
+	// Then inverse the two list
+	for(int i=0;i<n/2;i++){
+		L.data[i] += L.data[n-i-1];
+		L.data[n-i-1] = L.data[i] - L.data[n-i-1];
+		L.data[i] -= L.data[n-i-1];
+	}
+	for(int i=0;i<m/2;i++){
+		L.data[i+n] += L.data[L.length-i-1];
+		L.data[L.length-i-1] = L.data[i+n] - L.data[L.length-i-1];
+		L.data[i+n] -= L.data[L.length-i-1];
+	}
+
+	return true;
+}
+
+/* 9 10 11 12 13 14*/
 
 // for test
 int main(void){
-	SqList list;
-	InitList(list);
-	list.data[0] = 0;
-	list.data[1] = 1;
-	list.data[2] = 1;
-	list.data[3] = 3;
-	list.length = 4;
-	DeleteAllX2(list,1);
-	for(int i=0;i<list.length;i++)
-		printf("%d",list.data[i]);
+	SqList A;
+	InitList(A);
+	A.data[0] = 0;
+	A.data[1] = 1;
+	A.data[2] = 2;
+	A.data[3] = 3;
+	A.data[4] = 4;
+	A.data[5] = 5;
+	A.data[6] = 6;
+	A.data[7] = 7;
+	A.data[8] = 8;
+	A.data[9] = 9;
+	A.length = 10;
+
+	SqList B;
+	InitList(B);
+	B.data[0] = 1;
+	B.data[1] = 2;
+	B.data[2] = 3;
+	B.length = 3;
+
+	SqList C;
+	InitList(C);
+	C.data[0] = 1;
+	C.data[1] = 2;
+	C.data[2] = 3;
+	C.length = 3;
+
+	for(int i=0;i<A.length;i++)
+		printf("%d",A.data[i]);
+	printf("\n");
+	Exchange(A,8,2);
+	for(int i=0;i<A.length;i++)
+		printf("%d",A.data[i]);
 	printf("\n");
 }
 
