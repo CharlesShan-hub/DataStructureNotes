a31f2644e4d68717a79f961c063116c05f7ffadb
CharlesShan-hub <charles.shht@gmail.com>
2021-04-24 08:03:10 +0800

    更新到第九题

--- 线性表/线性表-练习题2.cpp
+++ 线性表/线性表-练习题2.cpp
@@ -155,28 +155,137 @@ bool DeleteMin(LinkList &L){
 // 第五题
 /* 编写在头节点的单链表就地逆置, 所谓“就地”是指是指辅助空间复杂度为O(1) */
 bool Inverse(LinkList &L1){
+	// 判空
 	if(L1->next==NULL)
 		return false;
 	// 采用头插法, 将链表逆置
-	// L1 - q
-	LNode* q = L1;
 	// L2 - p
-	LNode* L2 = L1;
-	LNode* p = L2;
+	LNode* L2 = (LNode*)malloc(sizeof(LNode));
+	if(L2==NULL)
+		return false;
+	LNode* p;
 	// s 来删除
-	LNode* s;
-	while(q->next != NULL){
-		// 赋值
-		printf("%d ",q->data);
-		p->next = q->next;
-		p->data = q->data;
-		// 删除本节点并迭代
-		s = p->next;
-		p = p->next;
-		q = q->next;
+	LNode* s = NULL;
+	while(L1->next!=NULL){
+		// 赋值给新节点 - p
+		//printf("-%d ",L1->next->data);
+		p = (LNode*)malloc(sizeof(LNode));
+		if(p==NULL)
+			return false;
+		p->data = L1->next->data;
+		p->next = L2->next;
+		L2->next = p;
+		// 删除老节点
+		s = L1->next;
+		if(s->next!=NULL){
+			L1->next = s->next;
+		}else{
+			L1->next = NULL;
+		}
 		free(s);
 	}
-	L1 = L2;
+	// 重新赋值
+	L1->next = L2->next;
+	free(L2);
+
+	return true;
+}
+
+// 第六题
+/* 有一个带有头节点的单链表L, 设计一个算法使其元素递增有序 */
+bool AddSort(LinkList &L){
+	// 判空或就一个元素
+	if(L->next==NULL||L->next->next==NULL)
+		return false;
+	// 递增 - 第一轮
+	// * -> * -> * -> * 
+	LNode *p = L;
+	int temp,count=0,in_count=0;
+	while(p->next->next!=NULL){
+		if(p->next->data > p->next->next->data){
+			temp = p->next->next->data;
+			p->next->next->data = p->next->data;
+			p->next->data = temp;
+		}
+		p = p->next;
+		count++;
+	}
+	for(int i=count; i>0; i--){
+		p = L;
+		in_count = i;
+		while(in_count){
+			if(p->next->data > p->next->next->data){
+				temp = p->next->next->data;
+				p->next->next->data = p->next->data;
+				p->next->data = temp;
+			}
+			p = p->next;
+			in_count--;
+		}
+	}
+	return true;
+}
+
+// 第七题
+/* 设在一个带表头节点的单链表中所有元素节点的数据之无序, 试编写一个函数,
+ 删除表中所有介于给定的两个值(只为函数参数给出)之间的元素的元素(若存在). */
+bool DeleteInXY(LinkList &L,int min,int max){
+	if(min>=max)
+		return false;
+	if(L->next==NULL)
+		return false;
+	// 开始删除!
+	LNode *s, *p=L;
+	while(p->next!=NULL){
+		if(p->next->data > min && p->next->data < max){
+			s = p->next;
+			p->next = p->next->next;
+			free(s);
+		}else{
+			p = p->next;
+		}
+	}
+	return false;
+}
+
+// 第八题
+/* 给定两个单链表, 编写算法找出两个链表的公共个节点 */
+// (L1)*-*-*-*-*-*-*-*-*
+//     (L2)*-*-*-*/
+bool FindSharedNode(LinkList L1, LinkList L2, LNode* public_){
+	// 先看是否有公共节点 - 分别计算两个长度
+	if(L1->next==NULL || L2->next==NULL)
+		return false;
+	int len1 = 0,len2 = 0;
+	LNode *p = L1, *q = L2;
+	for(len1=0; p->next!=NULL; len1++,p=p->next);
+	for(len2=0; q->next!=NULL; len2++,q=q->next);
+	printf("%d %d",len1,len2);
+	if(len1 == len2 && p != q)
+		return false;
+	// 有公共节点的情况
+	// Step1 - 先对其两链表
+	p = L1, q = L2;
+	int count = 0;
+	if(len1 > len2){
+		for(int i=0;i<len1-len2;i++)
+			p=p->next;
+		count = len1 - len2;
+	}
+	else{
+		for(int i=0;i<len1-len2;i++)
+			q=q->next;
+		count = len2 - len1;
+	}
+	// Step2 - 从对齐处开始寻找
+	for(int i=count; i>0; i--){
+		if(q->next == p->next && q->next!=NULL)
+			public_ = p->next;
+		else{
+			p = p->next;
+			q = q->next;
+		}
+	}
 	return true;
 }
 
@@ -227,10 +336,52 @@ int main(){
 	InitListNH(L5);
 	GenerateList(L5);
 	PrintList(L5);
-	DeleteNode(L5,false);
+	Inverse(L5);
 	PrintList(L5);
+	DeleteNode(L5,false);
 
-	// 
+	// 第六题
+	printf("\n\n第六题\n");
+	LinkList L6;
+	InitListNH(L6);
+	GenerateList(L6);
+	PrintList(L6);
+	AddSort(L6);
+	PrintList(L6);
+	DeleteNode(L6,false);
+
+	// 第七题
+	printf("\n\n第七题\n");
+	LinkList L7;
+	InitListNH(L7);
+	GenerateList(L7);
+	PrintList(L7);
+	DeleteInXY(L7,3,7);
+	PrintList(L7);
+	DeleteNode(L7,false);
+
+	// 第八题
+	printf("\n\n第八题\n");
+	LinkList L8_1,L8_2,L8_3;
+	InitListNH(L8_1);
+	InitListNH(L8_2);
+	GenerateList(L8_1);
+	GenerateList(L8_2);
+	FindSharedNode(L8_1,L8_2,L8_3);
+	DeleteNode(L8_1,false);
+	DeleteNode(L8_2,false);
+
+	// 第九题
+	printf("\n\n第九题\n");
+	LinkList L9;
+	InitListNH(L9);
+	GenerateList(L9);
+	PrintList(L9);
+	DeleteInXY(L9,3,7);
+	PrintList(L9);
+	DeleteNode(L9,false);
+
+	return 0;
 }
 
 
